6dcd25e7da919f9e4912d1fdfeeff9713975cc4c
shokhDeveloper <shohijahonmusinkulov@gmail.com>
2025-05-06 17:38:17 +0300

    Add users get request

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const {config} = require("dotenv");
 config()
 const express = require("express");
+const fs = require("fs/promises");
+const path = require("path");
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello world"))
+
+app.get("/users", async (req, res) => {
+    let users = await fs.readFile(path.join(process.cwd(), "db", "users.json"), "utf-8");
+    users = JSON.parse(users);
+    return res.json(users)
+})
 
 let PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on ${PORT}-port`));
